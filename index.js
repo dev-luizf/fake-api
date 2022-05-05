e20fabacd8cc59a6ca8f6ec0a93bc2ac944e9df6
devluizf <luizfurtadomkt@hotmail.com>
2022-05-05 11:49:53 -0300

    feat: implementa mock de login e mock de produtos

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const PORT = 3001;
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({
-  noCors: true
-});
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
 server.use(jsonServer.bodyParser);
+
+server.post("/sign-in", (req, res) => {
+  res.jsonp({
+    name: "Fulano da Silva",
+    email: "email@dominio.com",
+    role: "customer",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+  });
+});
+
+server.use(router);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running at port ${PORT}`);
